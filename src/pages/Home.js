835e4af8e0730bbb68d929e3539aeb0c14c139f0
PhilouLaudun <77538675+PhilouLaudun@users.noreply.github.com>
2022-05-20 14:10:39 +0200

    Préparation des pages : mise en forme contenu page d'acceuil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,15 +15,26 @@ const Home = () => {
 
         {/*Contenu de la page */}
         <div className="page-main">
-          <div className="main-content">
-            <h1>Page Accueuil</h1>
+          <div className="homeContent">
+            <img
+              src="../assets/img/camion.jpg"
+              alt="Camion"
+              className="photocamion"
+            />
+            <h1 className="hometitre">L’ Outil de Business intelligent propriétaire</h1>
+            <h2 className="hometexte">:
+              Tarifs-Transports vous propose de calculer de façon simple les
+              prix de revient des transports et de gérer ainsi leurs tarifs tout
+              en restant compétitif et rentable. Il permet de répondre
+              rapidement aux appels d’offres pour que tous les collaborateurs
+              puissent remettre un tarif mais également de préparer facilement
+              la révision des tarifs.
+            </h2>
           </div>
- 
         </div>
         <div className="footerPage">
           <Footer />
         </div>
-        
       </div>
     </main>
   );
